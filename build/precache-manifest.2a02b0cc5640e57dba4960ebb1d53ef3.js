ca2303f0713dc3e5ed1229d374c12ac811222a40
devon.chau <devon.chau@clui.com>
2019-11-22 16:05:55 +1000

    init project

---
+++ build/precache-manifest.2a02b0cc5640e57dba4960ebb1d53ef3.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c467f9df9ce51090e09e",
+    "url": "./css/main.css"
+  },
+  {
+    "revision": "1539d111461e52991895d18e16a65c27",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c467f9df9ce51090e09e",
+    "url": "./js/bundle.js"
+  }
+]);
